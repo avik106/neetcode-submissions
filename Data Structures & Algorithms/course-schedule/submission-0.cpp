f46d1e067e277e64c1d7c9be19608bcf57cfd7b2
Avik Sarkar <avik26122@gmail.com>
2026-08-29 22:27:39 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> indegree(numCourses,0);
+        vector<vector<int>> adj(numCourses);
+        for(auto& pre:prerequisites){
+            indegree[pre[1]]++;
+            adj[pre[0]].push_back(pre[1]);
+        }
+        queue<int> q;
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        int ans=0;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            ans++;
+            for(int it:adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        return ans==numCourses;
+    }
+};
